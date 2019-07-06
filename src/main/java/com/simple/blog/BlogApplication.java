dc871fbcd383c815f5b6ea221821db32cc93b874
songning <1457065857@qq.com>
2019-07-06 11:13:27 +0800

    后台配置

--- src/main/java/com/simple/blog/BlogApplication.java
+++ src/main/java/com/simple/blog/BlogApplication.java
@@ -3,6 +3,9 @@ package com.simple.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author sn
+ */
 @SpringBootApplication
 public class BlogApplication {
 
