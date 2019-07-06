1d9208f9b08b88db429d992ec4eb3863c901370d
songning <1457065857@qq.com>
2019-07-06 20:46:09 +0800

    controller

---
+++ src/main/java/com/simple/blog/controller/BloggerController.java
@@ -0,0 +1,14 @@
+package com.simple.blog.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sn
+ */
+@RestController
+@Slf4j
+@RequestMapping(value = "/blogger")
+public class BloggerController {
+}
