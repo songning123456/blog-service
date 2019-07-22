ff8df613ac59ee7c1499fd78f0ef7a9fa496f270
songning <songning@kedacom.com>
2019-07-22 08:36:31 +0800

    驼峰下划线命名转换

--- src/test/java/com/simple/blog/BlogApplicationTests.java
+++ src/test/java/com/simple/blog/BlogApplicationTests.java
@@ -1,16 +1,31 @@
 package com.simple.blog;
 
+import com.simple.blog.util.NameConvertUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void nameConvert() {
+        String str1 = "createTime";
+        String str2 = "create_time";
+
+        String result1 = NameConvertUtil.camelToUnderline(str1);
+        String result2 = NameConvertUtil.underlineToCamel(str2);
+
+        log.info("驼峰转下划线命名: {}", result1);
+        log.info("下划线转驼峰: {}", result2);
     }
 
 }
